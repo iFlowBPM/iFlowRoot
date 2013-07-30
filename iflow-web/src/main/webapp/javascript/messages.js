c79e0bcdc251230af61954433f3b009aa73bd54b
Infosistema <Infosistema@10.4.4.52>
2013-07-30 11:19:04 +0100

    Set the default Language to English

--- iflow-web/src/main/webapp/javascript/messages.js
+++ iflow-web/src/main/webapp/javascript/messages.js
@@ -1,8 +1,8 @@
 messages = {
-copy_clip_error: 'Erro ao copiar os dados para o clipboard.',
-copy_clip_success: 'Os dados foram copiados para o clipboard\n\n',
-confirm_tpl_delete: 'Tem a certeza que quer remover este Fluxo Exemplo?\nEsta acção é irreversível!',
-mark_msg_error: 'Não foi possível marcar a mensagem como lida.',
+copy_clip_error: 'Could not copy selected data into clipboard.',
+copy_clip_success: 'Following info was copied to your clipboard:\n\n',
+confirm_tpl_delete: 'Are you sure you want to remove this Sample Flow??\nThis is irreversible!',
+mark_msg_error: 'It was not possible to mark message as read.',
 // just if you see the following string correcly, maybe its OK
 dull_utf8_string: 'é'
 };
