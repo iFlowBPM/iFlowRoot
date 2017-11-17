fa2031cca20254219378139c0b1cb3dcc3ef9ea4
Pedro Gonçalves <pedro.goncalves@uniksystem.pt>
2017-11-17 09:40:28 +0000

    Alteração LibraryMarshaller.java -> A alteração efectuada garante que o editor lê as bibliotecas
    PS - Esta biblioteca está a ser injectada no jar do editor directamente porque a alteração efectuada não vai de acordo com as politicas de segurança do PARI PERSI

--- iflow-api/src/main/java/pt/iflow/api/xml/LibraryMarshaller.java
+++ iflow-api/src/main/java/pt/iflow/api/xml/LibraryMarshaller.java
@@ -44,16 +44,21 @@ public class LibraryMarshaller {
 
 	public static XmlLibrary unmarshal(InputStream inStream) throws Exception 
 	{
-		InputSource source = null;
-		source = new InputSource(inStream); // guess encoding from file			
-		SAXParserFactory spf = SAXParserFactory.newInstance();
+		//InputSource source = null;
+		//source = new InputSource(inStream); // guess encoding from file			
+		/*SAXParserFactory spf = SAXParserFactory.newInstance();
 		spf.setFeature("http://xml.org/sax/features/external-general-entities", false);
 		spf.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
-		spf.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
-		Source xmlSource = new SAXSource(spf.newSAXParser().getXMLReader(), source );
-		JAXBContext context = JAXBContext.newInstance(XmlLibrary.class);
-		Unmarshaller unmarshaller = context.createUnmarshaller();
-		return (XmlLibrary) unmarshaller.unmarshal(xmlSource);
+		spf.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);*/
+		//Source xmlSource = new SAXSource(spf.newSAXParser().getXMLReader(), source );
+		//JAXBContext context = JAXBContext.newInstance(XmlLibrary.class);
+		//Unmarshaller unmarshaller = context.createUnmarshaller();
+		//return (XmlLibrary) unmarshaller.unmarshal(xmlSource);
+		
+		JAXBContext context = JAXBContext.newInstance(new Class[] { XmlLibrary.class });
+	    Unmarshaller unmarshaller = context.createUnmarshaller();
+	    
+	    return (XmlLibrary)unmarshaller.unmarshal(inStream);
 		
 	}
 
